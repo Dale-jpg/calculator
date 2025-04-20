8cdd92c822288341e15ef3a0d6aa0cee9aec9874
Dale-jpg <echols.dale@gmail.com>
2025-04-20 12:49:31 -0400

    Created variables for the document objects
    Changes to be committed:
    	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // JavaScript goes here
 
+// const display = document.querySelector('#display');
+// const numberOne = document.querySelector('#numberOne');
+// const numberTwo = document.querySelector('#numberTwo');
+// const numberThree = document.querySelector('#numberThree');
+// const numberFour = document.querySelector('#numberFour');
+// const numberFive = document.querySelector('#numberFive');
+// const numberSix = document.querySelector('#numberSix');
+// const numberSeven = document.querySelector('#numberSeven');
+// const numberEight = document.querySelector('#numberEight');
+// const numberNine = document.querySelector('#numberNine');
+// const operatorAdd = document.querySelector("add");
+// const operatorSubtract = document.querySelector("subtract");
+// const operatorMultiply = document.querySelector("multiply");
+// const operatorDivide = document.querySelector("divide");
+// const operatorEqual = document.querySelector("equal");
+// const clear = document.querySelector("clear");
+
+const display = document.querySelector('#display');
+const digitButtons = document.querySelectorAll('.digit');
+const operatorButtons = document.querySelector('.operator');
+const clear = document.querySelector("clear");
+
+
+
 let firstNumber;
 let operator;
 let secondNumber; 
