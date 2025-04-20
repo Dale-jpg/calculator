f7546031b45123079f57428a381a93c58fae1c19
Dale-jpg <echols.dale@gmail.com>
2025-04-20 13:21:34 -0400

    Created listener and function for display
    digit clicked is stored in an array and the array
    is displayed on the display through another
    function.
    
    Changes to be committed:
    	modified:   script.js

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 // JavaScript goes here
 
-// const display = document.querySelector('#display');
-// const numberOne = document.querySelector('#numberOne');
-// const numberTwo = document.querySelector('#numberTwo');
-// const numberThree = document.querySelector('#numberThree');
-// const numberFour = document.querySelector('#numberFour');
-// const numberFive = document.querySelector('#numberFive');
-// const numberSix = document.querySelector('#numberSix');
-// const numberSeven = document.querySelector('#numberSeven');
-// const numberEight = document.querySelector('#numberEight');
-// const numberNine = document.querySelector('#numberNine');
-// const operatorAdd = document.querySelector("add");
-// const operatorSubtract = document.querySelector("subtract");
-// const operatorMultiply = document.querySelector("multiply");
-// const operatorDivide = document.querySelector("divide");
-// const operatorEqual = document.querySelector("equal");
-// const clear = document.querySelector("clear");
 
 const display = document.querySelector('#display');
 const digitButtons = document.querySelectorAll('.digit');
-const operatorButtons = document.querySelector('.operator');
+const operatorButtons = document.querySelectorAll('.operator');
 const clear = document.querySelector("clear");
 
 
-
+const displayArray = [];
+let input;
 let firstNumber;
 let operator;
 let secondNumber; 
+
+digitButtons.forEach(button => button.addEventListener("click",()=> {
+    input = Number(button.textContent);
+    displayArray.push(input);
+    populateDisplay(displayArray);
+}))
+
+function populateDisplay () {
+    display.textContent =  displayArray.join("");
+    return display.textContent
+}
 
 function add(a,b) {
     return a + b;
