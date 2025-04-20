0cabe57fb6f556f01f11da1db6d18f8adea93f4b
Dale-jpg <echols.dale@gmail.com>
2025-04-20 11:35:13 -0400

    created operate function
    conditional statement to determine what calculator
    function to call on for the numbers provided
    
    Changes to be committed:
    	modified:   script.js

--- script.js
+++ script.js
@@ -20,10 +20,29 @@ function divide (a,b) {
     return a / b;
 }
 
-console.log(add(4,2));
-console.log(subtract(4,2));
-console.log(multiply(4,2));
-console.log(divide(4,2));
+// console.log(add(4,2));
+// console.log(subtract(4,2));
+// console.log(multiply(4,2));
+// console.log(divide(4,2));
+
+function operate (operator,firstNumber,secondNumber) {
+    if (operator == 'add') {
+        return add(firstNumber,secondNumber);
+    } else if (operator == 'subtract') {
+        return subtract(firstNumber,secondNumber);
+    } else if (operator == 'multiply') {
+        return multiply(firstNumber,secondNumber);
+    } else if (operator == 'divide') {
+        return divide (firstNumber,secondNumber);
+    } else {
+        
+    }
+}
+
+console.log(operate('add',5,3));
+console.log(operate('subtract',5,3));
+console.log(operate('multiply',5,3));
+console.log(operate('divide',5,3));
 
 
 
