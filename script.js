08ff5f40a762a03ef16bd0bb2616a0303b910fe3
Dale-jpg <echols.dale@gmail.com>
2025-04-20 15:17:14 -0400

    created function to store number when number button pressed.
    implementing second operator logic to avoid issues.
    
    Changes to be committed:
    	modified:   script.js

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ const equalButton = document.querySelector(".equal");
 const clearButton = document.querySelector("clear");
 
 
-let displayString ;
-let input;
-let firstNumber;
-let operator;
-let secondNumber; 
+
+let displayString = '0';
+let firstNumber = null;
+let secondNumber = null;
+let firstOperator = null;
+let secondOperator = null;
+result = null; 
 
 digitButtons.forEach(button => button.addEventListener("click",()=> {
-    input = Number(button.textContent);
+    inputNumber(button.textContent);
     populateDisplay(displayString);
 }))
 
@@ -47,6 +49,32 @@ equalButton.addEventListener("click",() => {
     populateDisplay(displayString);
 
 })
+
+function inputNumber(number) {
+    if (firstNumber === 0) {
+        if (displayString === '0' || displayValue === 0) {
+            // 1st click - handles first operand input
+            displayString = number;
+        } else if(displayString === firstNumber) {
+            //starts new operation after inputEquals()
+            displayString = number;
+        } else {
+            displayString+= number;
+        }
+    } else {
+        //3rd click - inputs to secondNumber
+        if(displayString === firstNumber) {
+            displayString = number;
+        } else {
+            displayString += number;
+        }
+    }
+}
+
+function inputOperator(operator){
+    // 2nd click - handles first operator input
+    operator = 
+}
 
 function populateDisplay () {
     display.textContent =  displayString;
